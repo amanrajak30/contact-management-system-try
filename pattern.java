af41071eb0e005100b69b8d495f8a0cbc4f50a17
amanrajak30 <amanrajak30>
2025-04-25 19:54:47 +0530

    Your commit message here

---
+++ pattern.java
@@ -0,0 +1,18 @@
+
+class pattern
+{
+    public static void main (String args[])
+    {
+        int i; int  j;int c=1;
+        for(i=0;i<5;i++)
+        {
+            for(j=0;j<=i;j++)
+            {
+              System.out.print(" "+c+" ");
+              c++;
+            }
+            System.out.println();
+        }
+
+    }
+}
